e7f04815b0489ef6e45fe2f5460a2e01a2edaf59
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-09 18:58:46 +0300

    Fanomboana

---
+++ base.sql
@@ -0,0 +1,19 @@
+create database Projet_S4;
+use Projet_S4;
+
+create table utilisateur(
+    id_utilisateur int primary key auto_increment,
+    Nom varchar(20),
+    Prenom varchar(10),
+    email varchar(20),
+    mdp varchar(30)
+);
+
+create table inscription(
+    id_inscription int primary key auto_increment,
+    Nom varchar(20),
+    Prenom varchar(10),
+    email varchar(20),
+    mdp varchar(30),
+    Remdp varchar(30)
+);
