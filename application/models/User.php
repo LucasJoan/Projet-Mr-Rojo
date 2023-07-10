c45453d48efdd2a675897e9239eb984fb2780081
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-10 12:14:42 +0300

    Login mbol tsy mande

---
+++ application/models/User.php
@@ -0,0 +1,29 @@
+<?php 
+    class User extends CI_Model
+      {
+            private $idUser;
+            private $nom;
+            private $email;
+            private $motDePasse;
+            private $identification;
+      public function login()
+      {
+        $this->db->where('nom', $this->getNom());
+        $this->db->where('Prenom', $this->getPrenom());
+        $this->db->where('email', $this->getEmail());
+        $this->db->where('mdp', $this->getMotDePasse());
+        $query = $this->db->get('utilisateur');
+
+        foreach ($query->result() as $row) {
+            $utilisateur = new utilisateur();
+            $utilisateur->setId_utilisateur($row->id_utilisateur);
+            $utilisateur->setNom($row->nom);
+            $utilisateur->setEmail($row->email);
+            $utilisateur->setMotDePasse($row->motDePasse);
+            $utilisateur->setIdentification($row->identification);
+            return $user;
+        }
+        return null;
+      }
+}
+?>
