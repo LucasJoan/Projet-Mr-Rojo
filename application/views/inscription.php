dad82112f8e662fe608e17f43b848386e873892f
Lucas <Lucas.rajoelina@gmail.com>
2023-07-10 11:50:27 +0300

    Update inscription.php

--- application/views/inscription.php
+++ application/views/inscription.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/inscription.css">
+    <link rel="stylesheet" href="<?php echo base_url('Projet-Mr-Rojo/assets/inscription.css')?>">
     <title>Examen Web</title>
 </head>
 <body>
