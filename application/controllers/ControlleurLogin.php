17a545e8ea21c281a2c293944dd559ab3f0bc93a
SylviaRakotoson <sylyrakotoson@gmail.com>
2023-07-10 15:27:49 +0300

    login mbola tsy izy

--- application/controllers/ControlleurLogin.php
+++ application/controllers/ControlleurLogin.php
@@ -1,26 +1,27 @@
 <?php
-    class ControllerLogin extends CI_Controller
-      {
-            public function __construct() 
-            {
-                  parent::__construct();
-            }
-            public function Login()
-        {
-            $this->load->library('session');
-            $this->load->helper('url');
-            $email = $this->input->post('email');
-            $password = $this->input->post('password');
-            $user = new User(null,null,$email,$password,null);
-            $user = $user->login();
-            if($user != null){
-                $this->session->set_userdata('idUser',$user->getIdUser());
-            }
-            if($this->session->userdata('idUser') == null){
-                $this->load->view('Projet-Mr-Rojo/application/views/index');
-            }else{
-                redirect('');
-            }
+class ControllerLogin extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->helper('url');
+        $this->load->model('User');
+    }
+
+    public function login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $user = new User(null, null, $email, $password, null);
+        $user = $user->login();
+
+        if ($user != null) {
+            $this->session->set_userdata('idUser', $user->getIdUser());
+            redirect('');
+        } else {
+            $this->load->view('Projet-Mr-Rojo/views/index');
         }
-      }
+    }
+}
 ?>
--- application/models/User.php
+++ application/models/User.php
@@ -1,27 +1,80 @@
-<?php 
-    class User extends CI_Model
-      {
-            private $idUser;
-            private $nom;
-            private $email;
-            private $motDePasse;
-            private $identification;
-      public function login()
-      {
+<?php
+class User extends CI_Model
+{
+    private $idUser;
+    private $nom;
+    private $email;
+    private $motDePasse;
+    private $identification;
+
+    public function login()
+    {
         $this->db->where('email', $this->getEmail());
         $this->db->where('mdp', $this->getMotDePasse());
         $query = $this->db->get('utilisateur');
 
         foreach ($query->result() as $row) {
-            $utilisateur = new utilisateur();
-            $utilisateur->setId_utilisateur($row->id_utilisateur);
-            $utilisateur->setNom($row->nom);
+            $utilisateur = new User();
+            $utilisateur->setIdUser($row->id_utilisateur);
+            $utilisateur->setNom($row->Nom);
             $utilisateur->setEmail($row->email);
-            $utilisateur->setMotDePasse($row->motDePasse);
+            $utilisateur->setMotDePasse($row->mdp);
             $utilisateur->setIdentification($row->identification);
-            return $user;
+            return $utilisateur;
         }
         return null;
-      }
+    }
+
+    // Getter and Setter methods for the properties
+
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser($idUser)
+    {
+        $this->idUser = $idUser;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getMotDePasse()
+    {
+        return $this->motDePasse;
+    }
+
+    public function setMotDePasse($motDePasse)
+    {
+        $this->motDePasse = $motDePasse;
+    }
+
+    public function getIdentification()
+    {
+        return $this->identification;
+    }
+
+    public function setIdentification($identification)
+    {
+        $this->identification = $identification;
+    }
 }
 ?>
